c035f0de14ba50cc02a6b8ac6fceafb5cf173895
Chriss6202 <62573087+Chriss6202@users.noreply.github.com>
2022-06-28 16:57:44 -0500

    Update config.js

--- petbookApi/database/config.js
+++ petbookApi/database/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 var debug = require('debug')('PetBook:server');
 
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/PetBook")
+  .connect(process.env.MONGO_URI || "mongodb://188.166.91.212:3000/petbook")
   .then(
     () => {
       debug("Database connected ");
